d8204bc3f679e9f47185902ed20abb0db6b6aafd
dk2404 <diliprdy26@gmail.com>
2020-05-31 11:21:42 +0800

    init

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,18 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
 from flask_login import LoginManager 
-
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+admin = Admin(app,name='Control Panel')
 login = LoginManager(app)
 login.login_view = 'login'
+
+#admin.add_view(ModelView())
 
 from app import routes, models
